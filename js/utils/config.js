4c602fb3f9847843a6dfc4e82bfde842a8dda245
COURTESYCOIL <forbusiness6886543@gmail.com>
2026-08-01 00:28:22 +0300

    Auto-deploy update from IDE

--- js/utils/config.js
+++ js/utils/config.js
@@ -9,10 +9,11 @@ let baseUrl = window.location.origin;
 if (window.location.origin.includes('localhost') || window.location.origin.includes('127.0.0.1')) {
     baseUrl = 'http://localhost:8000';
 } else if (window.location.origin.includes('vercel.app') || window.location.origin.includes('github.io')) {
-    baseUrl = 'https://reaching-unique-list-top.trycloudflare.com';
+    baseUrl = 'https://widespread-marilyn-monkey-agreed.trycloudflare.com';
 }
 
 export const API_BASE_URL = window.PHRYCO_API_URL || baseUrl;
+
 
 
 
