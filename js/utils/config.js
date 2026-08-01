1bb1a3aed77253f95e196284f3fe4cb1cbec481c
COURTESYCOIL <forbusiness6886543@gmail.com>
2026-08-01 22:57:17 +0300

    Auto-deploy update from IDE

--- js/utils/config.js
+++ js/utils/config.js
@@ -9,10 +9,11 @@ let baseUrl = window.location.origin;
 if (window.location.origin.includes('localhost') || window.location.origin.includes('127.0.0.1')) {
     baseUrl = 'http://localhost:8000';
 } else if (window.location.origin.includes('vercel.app') || window.location.origin.includes('github.io')) {
-    baseUrl = 'https://dogs-aquatic-veterans-titled.trycloudflare.com';
+    baseUrl = 'https://flashers-argue-arise-lets.trycloudflare.com';
 }
 
 export const API_BASE_URL = window.PHRYCO_API_URL || baseUrl;
+
 
 
 
